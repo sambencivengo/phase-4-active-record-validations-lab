fd6cdbe81dd6448fe53665dd59f32d348532a6e4
Sam Bencivengo <sambencivengo@gmail.com>
2021-11-28 15:35:06 -0500

    commit tree 16ff2192ac2738d7588006c7f0c9a508b5ffde58

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Ficiton'] }
+  validate :clickbait?
+
+  def clickbait?
+    if title.to_s.include?("Won't Believe")
+         true
+    elsif title.to_s.include?( "Secret")
+         true
+    elsif title.to_s.include?("Top #{0..9}")
+        true
+    elsif title.to_s.include?("Guess")
+         true
+    else
+        errors.add(:title, "not clickbait")
+    end
+ end
+
 end
